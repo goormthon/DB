88a4255ed0f0c3aaf4e14ed3132c8061a9398213
MinChangJeong <wjdalsckd777@naver.com>
2023-09-07 11:55:13 +0900

    first commit

---
+++ k83/configs/init.sql
@@ -0,0 +1,7 @@
+CREATE SCHEMA IF NOT EXISTS `groom` DEFAULT CHARACTER SET utf8mb4;
+
+GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY 'root' WITH GRANT OPTION;
+GRANT ALL ON krampoline.* TO 'root'@'localhost';
+FLUSH PRIVILEGES;
+
+USE `groom`;
